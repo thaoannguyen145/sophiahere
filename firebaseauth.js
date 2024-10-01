52fff94ac892759dca67eefee9ea08d551408096
Thao An Nguyen <thaoan.nguyen@studenti.unipd.it>
2024-10-01 18:38:15 +0200

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -42,8 +42,8 @@ signUp.addEventListener('click', (event) => {
         const user = userCredential.user;
         const userData = {
             email: email
-            //firstName: firstName,
-            //lastName: lastName
+            firstName: firstName,
+            lastName: lastName
         };
         showMessage('Account Created Successfully', 'signUpMessage');
         const docRef = doc(db, "users", user.uid);

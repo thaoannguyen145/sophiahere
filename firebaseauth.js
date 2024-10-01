9550a1940bcde901b706bc808c0f864d2adfe4fe
Thao An Nguyen <thaoan.nguyen@studenti.unipd.it>
2024-10-01 16:34:17 +0200

    Update firebaseauth.js

---
+++ firebaseauth.js
@@ -0,0 +1,91 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC5UQOZc3QGWb_A1gyPCEAOn_BMBEXOBas",
+    authDomain: "sophia-here.firebaseapp.com",
+    projectId: "sophia-here",
+    storageBucket: "sophia-here.appspot.com",
+    messagingSenderId: "226577895652",
+    appId: "1:226577895652:web:4507f2b7d85a7923e89cf1"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ 
+ //Display message to users in a specified HTML element (eg: Created account successfully or errors)
+ function showMessage(message, divId){
+    var messageDiv=document.getElementById(divId);
+    messageDiv.style.display="block";
+    messageDiv.innerHTML=message;
+    messageDiv.style.opacity=1;
+    setTimeout(function(){
+        messageDiv.style.opacity=0;
+    },5000);
+ }
+
+ // Sign-up
+ const signUp = document.getElementById('submitSignUp');
+signUp.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    //const firstName = document.getElementById('fName').value;
+    //const lastName = document.getElementById('lName').value;
+
+    const auth = getAuth();
+    const db = getFirestore();
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+        const userData = {
+            email: email
+            //firstName: firstName,
+            //lastName: lastName
+        };
+        showMessage('Account Created Successfully', 'signUpMessage');
+        const docRef = doc(db, "users", user.uid);
+        setDoc(docRef, userData)
+        .then(() => {
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.error("error writing document", error);
+        });
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode == 'auth/email-already-in-use') {
+            showMessage('Email Address Already Exists !!!', 'signUpMessage');
+        } else {
+            showMessage('unable to create User', 'signUpMessage');
+        }
+    });
+});
+
+// Sign-in
+const signIn = document.getElementById('submitSignIn');
+signIn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        showMessage('login is successful', 'signInMessage');
+        const user = userCredential.user;
+        localStorage.setItem('loggedInUserId', user.uid);
+        window.location.href = 'homepage.html';
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        if (errorCode === 'auth/invalid-credential') {
+            showMessage('Incorrect Email or Password', 'signInMessage');
+        } else {
+            showMessage('Account does not Exist', 'signInMessage');
+        }
+    });
+});

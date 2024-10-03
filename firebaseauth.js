40b7f0f41a118cee6224d9eb9917ac5ae58b09b4
Thao An Nguyen <thaoan.nguyen@studenti.unipd.it>
2024-10-03 19:14:40 +0200

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -78,6 +78,9 @@ signIn.addEventListener('click', (event) => {
         showMessage('login is successful', 'signInMessage');
         const user = userCredential.user;
         localStorage.setItem('loggedInUserId', user.uid);
+        // this is new code
+        localStorage.setItem('google_access_token', data.access_token); 
+        // this is new code
         window.location.href = 'homepage.html';
     })
     .catch((error) => {
